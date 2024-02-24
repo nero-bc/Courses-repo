e4fa683b477c108136b29e368230988ea3e369de
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-02-24 21:05:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6400468647:AAHd3WGvsIQWg47kBijOuxE1aM_8H1WUuFI"
-    API_ID = "10551685"
-    API_HASH = "477dff815fdc7cba803c1bc02c459d03"
-    AUTH_USERS = '1112773045,1975696269'
+    BOT_TOKEN = "6140739061:AAFh-3q1v1KyZbjtBQMpQfNiF-ImH4ahZhs"
+    API_ID = "25695562"
+    API_HASH = "0b691c3e86603a7e34aae0b5927d725a"
+    AUTH_USERS = '1895952308'
 
